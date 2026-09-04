0026594028d2261661f2c695fb7d82f6b5967015
ProtonDev-sys <sdu23h7dy98y87asd7a@gmail.com>
2026-09-04 22:13:20 +0100

    Remove changelog section from Updates settings

--- src/components/settings/tabs/index.ts
+++ src/components/settings/tabs/index.ts
@@ -7,8 +7,6 @@
 import "./styles.css";
 
 export * from "./BaseTab";
-export { default as ChangelogTab } from "./changelog";
-export { default as UpdaterTab } from "./changelog";
 export { default as PatchHelperTab } from "./patchHelper";
 export { default as PluginsTab } from "./plugins";
 export { openContributorModal } from "./plugins/ContributorModal";
@@ -16,4 +14,5 @@ export { openPluginModal } from "./plugins/PluginModal";
 export { default as BackupAndRestoreTab } from "./sync/BackupAndRestoreTab";
 export { default as CloudTab } from "./sync/CloudTab";
 export { default as ThemesTab } from "./themes";
+export { default as UpdaterTab } from "./updater";
 export { default as VencordTab } from "./vencord";
