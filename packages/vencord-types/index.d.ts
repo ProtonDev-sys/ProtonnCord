6be011b00f0283e5983280924ae9304a5f4d50d7
ProtonDev-sys <sdu23h7dy98y87asd7a@gmail.com>
2026-09-05 14:05:50 +0100

    Generate only public API types and validate the package

--- packages/vencord-types/index.d.ts
+++ packages/vencord-types/index.d.ts
@@ -1,5 +1,6 @@
 /* eslint-disable */
 
+/// <reference types="standalone-electron-types" />
 /// <reference path="Vencord.d.ts" />
 /// <reference path="globals.d.ts" />
 /// <reference path="modules.d.ts" />
